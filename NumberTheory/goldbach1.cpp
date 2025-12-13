5fca2fdd13307c969869deae99513e2809797cf2
ADIB3883 <adibhoq55@gmail.com>
2025-12-14 02:15:58 +0600

    end of season1

---
+++ NumberTheory/goldbach1.cpp
@@ -0,0 +1,75 @@
+///https://codeforces.com/problemset/problem/735/D
+#include<bits/stdc++.h>
+#define cmp(typ) complex<typ>
+#define vsort(vc) sort(vc.begin(),vc.end())
+#define vrsort(vc) sort(vc.rbegin(),vc.rend())
+#define srev(st) reverse(st.rbegin(),st.rend())
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define Yes cout<<"Yes\n"
+#define No cout<<"No\n"
+#define yes cout<<"yes\n"
+#define no cout<<"no\n"
+#define Case(a) cout<<"Case "<<a<<": "
+#define CaseN(a) cout<<"Case "<<a<<": "
+#define psum(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1)
+#define pxor(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1,[](LL x,LL y){return x^y;})
+#define pgcdLeft(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1,[](LL x,LL y){return __gcd(x,y);})
+#define pgdcRight(vc,sm) partial_sum(vc.rbegin(),vc.rend(),sm.begin()+1,[](LL x,LL y){return __gcd(x,y);})
+#define ll long long int
+#define dub double
+#define dubb long double
+#define pb push_back
+#define pi acos(-1)
+#define nl '\n'
+using namespace std;
+void solve()
+{
+    ll i,j,k,n,m,c=0,s=0,f=0,t,x,y,z;
+    string p;
+    cin>>n;
+    bool prime=true;
+    for(i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            prime=false;
+        }
+    }
+    if(prime)cout<<1<<'\n';
+    else if(n%2==0)
+    {
+        cout<<2<<'\n';
+    }
+    else
+    {
+        prime=true;
+        n-=2;
+    for(i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            prime=false;
+        }
+    }
+    if(prime)cout<<2<<'\n';
+    else
+    {
+        cout<<3<<'\n';
+    }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ll t;
+   // cin>>t;
+    t=1;
+    while(t--)
+    {
+     solve();     
+    }
+    return 0;
+}
+
+
+
