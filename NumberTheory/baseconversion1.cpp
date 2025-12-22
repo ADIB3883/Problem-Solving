f1143bf74f3f01679ef27dd77372948ef9a0ff4a
ADIB3883 <adibhoq55@gmail.com>
2025-12-23 03:14:22 +0600

    Shuru

---
+++ NumberTheory/baseconversion1.cpp
@@ -0,0 +1,51 @@
+///https://codeforces.com/contest/602/problem/A
+#include<bits/stdc++.h>
+#define cmp(typ) complex<typ>
+#define vsort(vc) sort(vc.begin(),vc.end())
+#define vrsort(vc) sort(vc.rbegin(),vc.rend())
+#define srev(st) reverse(st.rbegin(),st.rend())
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define Yes cout<<"Yes\n"
+#define No cout<<"No\n"
+#define yes cout<<"yes\n"
+#define no cout<<"no\n"
+#define Case(a) cout<<"Case "<<a<<":"
+#define CaseN(a) cout<<"Case "<<a<<":"
+#define psum(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1)
+#define pxor(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1,[](LL x,LL y){return x^y;})
+#define pgcdLeft(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1,[](LL x,LL y){return __gcd(x,y);})
+#define pgdcRight(vc,sm) partial_sum(vc.rbegin(),vc.rend(),sm.begin()+1,[](LL x,LL y){return __gcd(x,y);})
+#define ll long long int
+#define dub double
+#define dubb long double
+#define pi acos(-1)
+#define nl '\n'
+using namespace std;
+void solve() {
+   ll i,j,k,n,m,c=0,s=0,f=0,t,x,y,z;
+   string p;
+   ll n1=0,n2=0;
+   cin>>n>>k;
+   vector<ll>v1(n);
+   for(i=0;i<n;i++)cin>>v1[i];
+   for(i=n-1;i>=0;i--)n1+=(v1[i]*pow(k,n-1-i));
+   cin>>n>>k;
+   vector<ll>v2(n);
+   for(i=0;i<n;i++)cin>>v2[i];
+   for(i=n-1;i>=0;i--)n2+=(v2[i]*pow(k,n-1-i));
+   if(n1==n2)cout<<"=\n";
+   else if(n1>n2)cout<<">\n";
+   else cout<<"<\n";
+}
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+   ll t;
+   t=1;
+ //  cin>>t;
+   while(t--)
+   {
+     solve();
+   }
+   return 0;
+}
