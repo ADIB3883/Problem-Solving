c8cd8086b50fed0592920450e2d2593d1847d170
ADIB3883 <adibhoq55@gmail.com>
2026-01-18 01:45:28 +0600

    Edit

---
+++ Codeforces/1073/B.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#define cmp(typ) complex<typ>
+#define vsort(vc) sort(vc.begin(),vc.end())
+#define vrsort(vc) sort(vc.rbegin(),vc.rend())
+#define srev(st) reverse(st.rbegin(),st.rend())
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define Yes cout<<"Yes\n"
+#define No cout<<"No\n"
+#define yes cout<<"yes\n"
+#define no cout<<"no\n"
+#define Case(a) cout<<"Case "<<a<<":"
+#define CaseN(a) cout<<"Case "<<a<<":"
+#define psum(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1)
+#define pxor(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1,[](LL x,LL y){return x^y;})
+#define pgcdLeft(vc,sm) partial_sum(vc.begin(),vc.end(),sm.begin()+1,[](LL x,LL y){return __gcd(x,y);})
+#define pgdcRight(vc,sm) partial_sum(vc.rbegin(),vc.rend(),sm.begin()+1,[](LL x,LL y){return __gcd(x,y);})
+#define ll long long int
+#define dub double
+#define dubb long double
+#define pi acos(-1)
+#define nl '\n'
+using namespace std;
+ll mexxx(set<ll>s){
+    ll mexx=0;
+   // cout<<"aswsdwa";
+    while(s.count(mexx))mexx++;
+   // cout<<"a";
+    return mexx;
+}
+void solve() {
+   ll i,j,k,n,m,c=0,s=0,f=0,t,x,y,z;
+   string p;
+   cin>>n;
+   vector<ll>v(n+1);
+   for(i=1;i<=n;i++)
+   {
+    cin>>v[i];
+   }
+   sort(v.begin(),v.end());
+   for(i=1;i<n;i++)
+   {
+    set<ll>s1,s2;
+    for(j=1;j<=i;j++)
+    {
+        s1.insert(v[j]);
+    }
+    for(j=i+1;j<=n;j++)
+    {
+        s2.insert(v[j]);
+    }
+    if(mexxx(s1)==mexxx(s2))
+    {
+        NO;
+        return;
+    }
+   }
+   YES;
+}
+int main(){
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+   ll t;
+   t=1;
+   cin>>t;
+   while(t--)
+   {
+     solve();
+   }
+   return 0;
+}
